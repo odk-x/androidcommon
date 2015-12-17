fe688f2af8a9110c282424de2a56590103374752
Mitch Sundt <mitchellsundt@gmail.com>
2015-12-17 11:05:33 -0800

    Move file I/O functionality out of CsvUtil into PropertiesFileUtils

--- androidcommon_lib/src/main/java/org/opendatakit/common/android/utilities/CsvUtil.java
+++ androidcommon_lib/src/main/java/org/opendatakit/common/android/utilities/CsvUtil.java
@@ -59,6 +59,7 @@ import org.opendatakit.database.service.OdkDbHandle;
 
 import android.content.ContentValues;
 import android.os.RemoteException;
+import org.opendatakit.database.service.OdkDbInterface;
 
 /**
  * Various utilities for importing/exporting tables from/to CSV.
@@ -261,6 +262,7 @@ public class CsvUtil {
       OrderedColumns orderedDefns) throws RemoteException {
     File definitionCsv = new File(ODKFileUtils.getTableDefinitionCsvFile(appName, tableId));
     File propertiesCsv = new File(ODKFileUtils.getTablePropertiesCsvFile(appName, tableId));
+
     return writePropertiesCsv(db, tableId, orderedDefns, definitionCsv, propertiesCsv);
   }
 
@@ -279,139 +281,31 @@ public class CsvUtil {
       OrderedColumns orderedDefns, File definitionCsv, File propertiesCsv) throws RemoteException {
     WebLogger.getLogger(appName).i(TAG, "writePropertiesCsv: tableId: " + tableId);
 
-    // writing metadata
-    FileOutputStream out;
-    RFC4180CsvWriter cw;
-    OutputStreamWriter output = null;
-    try {
-      // emit definition.csv table...
-      out = new FileOutputStream(definitionCsv);
-      output = new OutputStreamWriter(out, CharEncoding.UTF_8);
-      cw = new RFC4180CsvWriter(output);
+    /**
+     * Since the md5Hash of the file identifies identical properties, ensure
+     * that the list of KVS entries is in alphabetical order.
+     */
+    List<KeyValueStoreEntry> kvsEntries =
+        context.getDatabase().getDBTableMetadata(appName, db, tableId, null, null, null);
+    for (int i = 0; i < kvsEntries.size(); i++) {
+      KeyValueStoreEntry entry = kvsEntries.get(i);
 
-      // Emit ColumnDefinitions
-
-      ArrayList<String> colDefHeaders = new ArrayList<String>();
-      colDefHeaders.add(ColumnDefinitionsColumns.ELEMENT_KEY);
-      colDefHeaders.add(ColumnDefinitionsColumns.ELEMENT_NAME);
-      colDefHeaders.add(ColumnDefinitionsColumns.ELEMENT_TYPE);
-      colDefHeaders.add(ColumnDefinitionsColumns.LIST_CHILD_ELEMENT_KEYS);
-
-      cw.writeNext(colDefHeaders.toArray(new String[colDefHeaders.size()]));
-      String[] colDefRow = new String[colDefHeaders.size()];
-
-      /**
-       * Since the md5Hash of the file identifies identical schemas, ensure that
-       * the list of columns is in alphabetical order.
-       */
-      for (ColumnDefinition cd : orderedDefns.getColumnDefinitions()) {
-        colDefRow[0] = cd.getElementKey();
-        colDefRow[1] = cd.getElementName();
-        colDefRow[2] = cd.getElementType();
-        colDefRow[3] = cd.getListChildElementKeys();
-        cw.writeNext(colDefRow);
-      }
-
-      cw.flush();
-      cw.close();
-
-      // emit properties.csv...
-      out = new FileOutputStream(propertiesCsv);
-      output = new OutputStreamWriter(out, CharEncoding.UTF_8);
-      cw = new RFC4180CsvWriter(output);
-
-      // Emit KeyValueStore
-
-      ArrayList<String> kvsHeaders = new ArrayList<String>();
-      kvsHeaders.add(KeyValueStoreColumns.PARTITION);
-      kvsHeaders.add(KeyValueStoreColumns.ASPECT);
-      kvsHeaders.add(KeyValueStoreColumns.KEY);
-      kvsHeaders.add(KeyValueStoreColumns.VALUE_TYPE);
-      kvsHeaders.add(KeyValueStoreColumns.VALUE);
-
-      /**
-       * Since the md5Hash of the file identifies identical properties, ensure
-       * that the list of KVS entries is in alphabetical order.
-       */
-      List<KeyValueStoreEntry> kvsEntries = context.getDatabase().getDBTableMetadata(appName, db, tableId,
-          null, null, null);
-      Collections.sort(kvsEntries, new Comparator<KeyValueStoreEntry>() {
-
-        @Override
-        public int compare(KeyValueStoreEntry lhs, KeyValueStoreEntry rhs) {
-          int outcome;
-          if (lhs.partition == null && rhs.partition == null) {
-            outcome = 0;
-          } else if (lhs.partition == null) {
-            return -1;
-          } else if (rhs.partition == null) {
-            return 1;
-          } else {
-            outcome = lhs.partition.compareTo(rhs.partition);
-          }
-          if (outcome != 0)
-            return outcome;
-          if (lhs.aspect == null && rhs.aspect == null) {
-            outcome = 0;
-          } else if (lhs.aspect == null) {
-            return -1;
-          } else if (rhs.aspect == null) {
-            return 1;
-          } else {
-            outcome = lhs.aspect.compareTo(rhs.aspect);
-          }
-          if (outcome != 0)
-            return outcome;
-          if (lhs.key == null && rhs.key == null) {
-            outcome = 0;
-          } else if (lhs.key == null) {
-            return -1;
-          } else if (rhs.key == null) {
-            return 1;
-          } else {
-            outcome = lhs.key.compareTo(rhs.key);
-          }
-          return outcome;
+      // replace all the choiceList entries with their choiceListJSON
+      if (entry.partition.equals(KeyValueStoreConstants.PARTITION_COLUMN) && entry.key
+          .equals(KeyValueStoreConstants.COLUMN_DISPLAY_CHOICES_LIST)) {
+        // exported type is an array -- the choiceListJSON
+        entry.type = ElementDataType.array.name();
+        if ((entry.value != null) && (entry.value.trim().length() != 0)) {
+          String choiceListJSON = context.getDatabase().getChoiceList(appName, db, entry.value);
+          entry.value = choiceListJSON;
+        } else {
+          entry.value = null;
         }
-      });
-
-      cw.writeNext(kvsHeaders.toArray(new String[kvsHeaders.size()]));
-      String[] kvsRow = new String[kvsHeaders.size()];
-      for (int i = 0; i < kvsEntries.size(); i++) {
-        KeyValueStoreEntry entry = kvsEntries.get(i);
-
-        // replace all the choiceList entries with their choiceListJSON
-        if ( entry.partition.equals(KeyValueStoreConstants.PARTITION_COLUMN) &&
-            entry.key.equals(KeyValueStoreConstants.COLUMN_DISPLAY_CHOICES_LIST) ) {
-          // exported type is an array -- the choiceListJSON
-          entry.type = ElementDataType.array.name();
-          if ((entry.value != null) && (entry.value.trim().length() != 0)) {
-            String choiceListJSON = context.getDatabase().getChoiceList(appName, db, entry.value);
-            entry.value = choiceListJSON;
-          } else {
-            entry.value = null;
-          }
-        }
-
-        kvsRow[0] = entry.partition;
-        kvsRow[1] = entry.aspect;
-        kvsRow[2] = entry.key;
-        kvsRow[3] = entry.type;
-        kvsRow[4] = entry.value;
-        cw.writeNext(kvsRow);
-      }
-      cw.flush();
-      cw.close();
-
-      return true;
-    } catch (IOException e) {
-      return false;
-    } finally {
-      try {
-        output.close();
-      } catch (IOException e) {
       }
     }
+
+    return PropertiesFileUtils.writePropertiesIntoCsv(appName, tableId, orderedDefns, kvsEntries,
+        definitionCsv, propertiesCsv);
   }
 
   private int countUpToLastNonNullElement(String[] row) {
@@ -434,141 +328,15 @@ public class CsvUtil {
    * that defined in the csv. It will then override all the KVS entries with
    * those present in the file.
    *
-   * @param importListener
    * @param tableId
    * @throws IOException
    * @throws RemoteException 
    */
-  public synchronized void updateTablePropertiesFromCsv(ImportListener importListener, String tableId)
+  public synchronized void updateTablePropertiesFromCsv(String tableId)
       throws IOException, RemoteException {
 
-    WebLogger.getLogger(appName).i(TAG, "updateTablePropertiesFromCsv: tableId: " + tableId);
-
-    List<Column> columns = new ArrayList<Column>();
-    List<KeyValueStoreEntry> kvsEntries = new ArrayList<KeyValueStoreEntry>();
-
-    // reading data
-    File file = null;
-    FileInputStream in = null;
-    InputStreamReader input = null;
-    RFC4180CsvReader cr = null;
-    try {
-      file = new File(ODKFileUtils.getTableDefinitionCsvFile(appName, tableId));
-      in = new FileInputStream(file);
-      input = new InputStreamReader(in, CharEncoding.UTF_8);
-      cr = new RFC4180CsvReader(input);
-
-      String[] row;
-
-      // Read ColumnDefinitions
-      // get the column headers
-      String[] colHeaders = cr.readNext();
-      int colHeadersLength = countUpToLastNonNullElement(colHeaders);
-      // get the first row
-      row = cr.readNext();
-      while (row != null && countUpToLastNonNullElement(row) != 0) {
-
-        String elementKeyStr = null;
-        String elementNameStr = null;
-        String elementTypeStr = null;
-        String listChildElementKeysStr = null;
-        int rowLength = countUpToLastNonNullElement(row);
-        for (int i = 0; i < rowLength; ++i) {
-          if (i >= colHeadersLength) {
-            throw new IllegalStateException("data beyond header row of ColumnDefinitions table");
-          }
-          if (ColumnDefinitionsColumns.ELEMENT_KEY.equals(colHeaders[i])) {
-            elementKeyStr = row[i];
-          }
-          if (ColumnDefinitionsColumns.ELEMENT_NAME.equals(colHeaders[i])) {
-            elementNameStr = row[i];
-          }
-          if (ColumnDefinitionsColumns.ELEMENT_TYPE.equals(colHeaders[i])) {
-            elementTypeStr = row[i];
-          }
-          if (ColumnDefinitionsColumns.LIST_CHILD_ELEMENT_KEYS.equals(colHeaders[i])) {
-            listChildElementKeysStr = row[i];
-          }
-        }
-
-        if (elementKeyStr == null || elementTypeStr == null) {
-          throw new IllegalStateException("ElementKey and ElementType must be specified");
-        }
-
-        columns.add(new Column(elementKeyStr, elementNameStr, elementTypeStr,
-            listChildElementKeysStr));
-
-        // get next row or blank to end...
-        row = cr.readNext();
-      }
-
-      cr.close();
-      try {
-        input.close();
-      } catch (IOException e) {
-      }
-      try {
-        in.close();
-      } catch (IOException e) {
-      }
-
-      file = new File(ODKFileUtils.getTablePropertiesCsvFile(appName, tableId));
-      in = new FileInputStream(file);
-      input = new InputStreamReader(in, CharEncoding.UTF_8);
-      cr = new RFC4180CsvReader(input);
-      // Read KeyValueStore
-      // read the column headers
-      String[] kvsHeaders = cr.readNext();
-      // read the first row
-      row = cr.readNext();
-      while (row != null && countUpToLastNonNullElement(row) != 0) {
-        String partition = null;
-        String aspect = null;
-        String key = null;
-        String type = null;
-        String value = null;
-        int rowLength = countUpToLastNonNullElement(row);
-        for (int i = 0; i < rowLength; ++i) {
-          if (KeyValueStoreColumns.PARTITION.equals(kvsHeaders[i])) {
-            partition = row[i];
-          }
-          if (KeyValueStoreColumns.ASPECT.equals(kvsHeaders[i])) {
-            aspect = row[i];
-          }
-          if (KeyValueStoreColumns.KEY.equals(kvsHeaders[i])) {
-            key = row[i];
-          }
-          if (KeyValueStoreColumns.VALUE_TYPE.equals(kvsHeaders[i])) {
-            type = row[i];
-          }
-          if (KeyValueStoreColumns.VALUE.equals(kvsHeaders[i])) {
-            value = row[i];
-          }
-        }
-        KeyValueStoreEntry kvsEntry = KeyValueStoreUtils.buildEntry(tableId, partition, aspect, key,
-                ElementDataType.valueOf(type), value);
-        kvsEntries.add(kvsEntry);
-        // get next row or blank to end...
-        row = cr.readNext();
-      }
-      cr.close();
-      try {
-        input.close();
-      } catch (IOException e) {
-      }
-      try {
-        in.close();
-      } catch (IOException e) {
-      }
-
-    } finally {
-      try {
-        if (input != null) {
-          input.close();
-        }
-      } catch (IOException e) {
-      }
-    }
+    PropertiesFileUtils.DataTableDefinition dtd = PropertiesFileUtils.readPropertiesFromCsv(appName,
+        tableId);
 
     OdkDbHandle db = null;
     try {
@@ -576,9 +344,9 @@ public class CsvUtil {
       db = context.getDatabase().openDatabase(appName);
 
       // Go through the KVS list and replace all the choiceList entries with their choiceListId
-      for ( KeyValueStoreEntry entry : kvsEntries ) {
+      for ( KeyValueStoreEntry entry : dtd.kvsEntries ) {
         if ( entry.partition.equals(KeyValueStoreConstants.PARTITION_COLUMN) &&
-             entry.key.equals(KeyValueStoreConstants.COLUMN_DISPLAY_CHOICES_LIST) ) {
+            entry.key.equals(KeyValueStoreConstants.COLUMN_DISPLAY_CHOICES_LIST) ) {
           // stored type is a string -- the choiceListId
           entry.type = ElementDataType.string.name();
           if ((entry.value != null) && (entry.value.trim().length() != 0)) {
@@ -590,8 +358,8 @@ public class CsvUtil {
         }
       }
 
-      ColumnList cols = new ColumnList(columns);
-      context.getDatabase().createOrOpenDBTableWithColumnsAndProperties(appName, db, tableId, cols, kvsEntries, true);
+      context.getDatabase().createOrOpenDBTableWithColumnsAndProperties(appName, db, tableId,
+          dtd.columnList, dtd.kvsEntries, true);
 
     } finally {
       if (db != null) {
@@ -629,7 +397,7 @@ public class CsvUtil {
       db = context.getDatabase().openDatabase(appName);
       if (!context.getDatabase().hasTableId(appName, db, tableId)) {
         if (createIfNotPresent) {
-          updateTablePropertiesFromCsv(importListener, tableId);
+          updateTablePropertiesFromCsv(tableId);
           if (!context.getDatabase().hasTableId(appName, db, tableId)) {
             return false;
           }
