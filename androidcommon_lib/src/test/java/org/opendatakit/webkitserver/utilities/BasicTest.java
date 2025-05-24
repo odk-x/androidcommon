136463850d2680d9c9495cde63f525ff7d9fdd8a
sonalgaud12 <sonalgaud12@gmail.com>
2025-05-24 14:23:35 +0530

    recovered basictest

---
+++ androidcommon_lib/src/test/java/org/opendatakit/webkitserver/utilities/BasicTest.java
@@ -0,0 +1,12 @@
+package org.opendatakit.webkitserver.utilities;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BasicTest {
+    @Test
+    public void basicTest() {
+        assertTrue(true);
+    }
+}
